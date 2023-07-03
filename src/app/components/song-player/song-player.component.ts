7a3477b87ea86ee442fde47aa63de2cef5e55ddb
DodyDharma440 <dharma.dody@yahoo.com>
2023-07-03 11:10:05 +0800

    custom slider volume and playback

--- src/app/components/song-player/song-player.component.ts
+++ src/app/components/song-player/song-player.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, range } from 'rxjs';
 import { Song, SongState } from 'src/app/models/song.model';
 import { State } from 'src/app/models/state.model';
 import { SongService } from 'src/app/services/song.service';
@@ -13,10 +13,20 @@ import { msToMinutes } from 'src/app/utils/time';
 export class SongPlayerComponent implements OnInit {
   @ViewChild('musicPlayer') audioRef: ElementRef<HTMLAudioElement> | null =
     null;
+
   @ViewChild('rangeInput') rangeInput: ElementRef<HTMLInputElement> | null =
     null;
+  @ViewChild('sliderThumb') sliderThumb: ElementRef<HTMLDivElement> | null =
+    null;
+  @ViewChild('sliderProgress')
+  sliderProgress: ElementRef<HTMLDivElement> | null = null;
+
   @ViewChild('volumeInput') volumeInput: ElementRef<HTMLInputElement> | null =
     null;
+  @ViewChild('volumeThumb') volumeThumb: ElementRef<HTMLDivElement> | null =
+    null;
+  @ViewChild('volumeProgress')
+  volumeProgress: ElementRef<HTMLDivElement> | null = null;
 
   songState$: Observable<SongState['playedSong']> = this.store.select(
     (store) => store.songs.playedSong
@@ -52,27 +62,40 @@ export class SongPlayerComponent implements OnInit {
     audio?.addEventListener('pause', () => (this.isPlaying = false));
     audio?.addEventListener('play', () => (this.isPlaying = true));
     audio?.addEventListener('timeupdate', (e) => {
-      const value = Number(
-        (e.target as HTMLAudioElement).currentTime.toString().split('.')[0]
-      );
-      this.currentTime = value * 1000;
-      this.rangeInput!.nativeElement.value = (
-        e.target as HTMLAudioElement
-      ).currentTime.toString();
+      const rangeValue = (e.target as HTMLAudioElement).currentTime.toString();
+      const timeValue = Number(rangeValue.split('.')[0]);
+      this.currentTime = timeValue * 1000;
+      this.rangeInput!.nativeElement.value = rangeValue;
+      this.onUpdateProgressUi(rangeValue);
     });
     audio?.addEventListener('volume', (e) => {
       const volume = (e.target as HTMLAudioElement).volume;
       this.volume = volume;
       this.volumeInput!.nativeElement.value = volume.toString();
     });
+
+    this.onUpdateVolumeUi((this.volume / 100).toString());
+  }
+
+  onUpdateProgressUi(value: string) {
+    const width = `${(Number(value) / 30) * 100}%`;
+    this.sliderProgress!.nativeElement.style.width = width;
+    this.sliderThumb!.nativeElement.style.left = width;
+  }
+
+  onUpdateVolumeUi(value: string) {
+    const width = `${(Number(value) / 100) * 100 * 100}%`;
+    this.volumeProgress!.nativeElement.style.width = width;
   }
 
   onChangeRange(rangeInput: HTMLInputElement) {
     this.audioRef!.nativeElement.currentTime = Number(rangeInput.value);
+    this.onUpdateProgressUi(rangeInput.value);
   }
 
   onChangeVolume(volumeInput: HTMLInputElement) {
     this.audioRef!.nativeElement.volume = Number(volumeInput.value);
+    this.onUpdateVolumeUi(volumeInput.value);
   }
 
   onTogglePlay() {
